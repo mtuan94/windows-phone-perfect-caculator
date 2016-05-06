06bb49e3058b80536be952f470d1269133d9797d
Tuanguyen <tuanguyen94@gmail.com>
2016-05-07 04:32:42 +0700

    requirement for caculate class

--- WP_Caculator/caculate.cs
+++ WP_Caculator/caculate.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//this class provide function to caculate
+//in put: Double
+//output: string ( to display)
+//      result and extention
+//example: 1/10000000 = 
+//      result: 1
+//      extent: 10^-7
+
+//todo: i dont care how your function work
+//i want your string output and i will paste it to textbox result
 namespace WP_Caculator
 {
     class caculate
